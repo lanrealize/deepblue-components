eb5124e12e9c412d5ca79d2d24587f879f3c8d8e
SunPengYuan <pengyuan.sun@siemens.com>
2024-01-12 22:07:23 +0800

    develop details top menu

---
+++ miniprogram/components/details-top-menu/details-top-menu.ts
@@ -0,0 +1,24 @@
+// components/details-top-menu/details-top-menu.ts
+Component({
+
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
